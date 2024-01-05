8092f1f45be03ab9ebda7a36a54b00e32be5ba30
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-01-05 22:42:38 +0000

    Refactor GitHub API and JSON API scripts

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
 """
-Module to access to the GitHub API and uses the information
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
 """
 import requests
 from requests.auth import HTTPBasicAuth
 from sys import argv
 
-
-def main(argv):
-    """
-    Script that takes your GitHub credentials (username and password) and
-    uses the GitHub API to display your id.
-    """
-    user = argv[1]
-    password = argv[2]
-    response = requests.get('https://api.github.com/user',
-                            auth=HTTPBasicAuth(user, password))
-    try:
-        profile_info = response.json()
-        print(profile_info['id'])
-    except:
-        print('None')
-
-if __name__ == "__main__":
-    main(argv)
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
 """
-Module  sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
 """
 import requests
 from sys import argv
 
-
-def main(argv):
-    """
-    Script that takes in a letter and sends a POST request to
-    http://0.0.0.0:5000/search_user with the letter as a parameter.
-    """
-    if len(argv) < 2:
-        q = ""
-    else:
-        q = argv[1]
-    payload = {'q': q}
-    url = "http://0.0.0.0:5000/search_user"
-    r = requests.post(url, data=payload)
+if __name__ == '__main__':
+    q = argv[1] if len(argv) == 2 else ""
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data={'q': q})
     try:
-        result = r.json()
-        if bool(result) is False:
+        r_dict = r.json()
+        id, name = r_dict.get('id'), r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
             print("No result")
         else:
-            print("[{}] {}".format(result['id'], result['name']))
-    except:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
         print("Not a valid JSON")
-
-if __name__ == "__main__":
-    main(argv)
