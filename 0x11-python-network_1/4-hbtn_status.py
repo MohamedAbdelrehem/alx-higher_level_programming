c09c245df7ecf3b14ab8376fbcd87d6eaaf2e627
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-01-05 22:37:19 +0000

    Refactor script to fetch and print response from URL

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 """
-Module using request that fetches https://intranet.hbtn.io/status
+Script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-
-def main():
-    """
-    Function that fetches https://intranet.hbtn.io/status
-    """
-    url = 'https://intranet.hbtn.io/status'
+if __name__ == '__main__':
+    url = "https://intranet.hbtn.io/status"
     r = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
-
-if __name__ == "__main__":
-    main()
+    text = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
