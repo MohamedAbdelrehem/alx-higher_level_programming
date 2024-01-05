89f4cab81db5dcd8399ec1fb7c0d1b6abd655afe
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-01-05 22:40:34 +0000

    Refactor script to simplify code and improve readability

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 """
-POST request to the passed URL with the email as a parameter
+Script that takes in a URL and an email address,
+sends a POST request to the passed URL
+with the email as a parameter,
+and finally displays the body of the response
 """
 import requests
 from sys import argv
 
-
-def main(argv):
-    """
-    Sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)
-    """
-    values = {'email': argv[2]}
-    url = argv[1]
-    r = requests.post(url, data=values)
+if __name__ == '__main__':
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
     print(r.text)
-
-if __name__ == "__main__":
-    main(argv)
