c2a779aede06c69235109f47ab07902d357bcf21
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-01-05 22:32:13 +0000

    Update 1-hbtn_header.py to fetch data from https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
+"""This script fetches data from
+    https://alx-intranet.hbtn.io/status
 """
-Displays the value of the X-Request-Id variable
-found in the header of the response
-"""
+
+
 import urllib.request
-from sys import argv
 
-
-def main(argv):
-    """
-    Method that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable
-    found in the header of the response
-    """
-    url = argv
-    with urllib.request.urlopen(url) as response:
-        headers = response.info()
-        print(headers['X-Request-Id'])
-    print("- utf8 content: OK$")
-
-if __name__ == "__main__":
-    main(argv[1])
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
