0830dd970bba0aaaaadae197e5aafe57bdbfa408
Mohamed Abdelrehem <mohamedabdelrehem32@gmail.com>
2024-01-05 22:38:51 +0000

    Refactor code to simplify and improve readability

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 """
-Displays the value of the X-Request-Id variable
-found in the header of the response
+Python script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
 """
 import requests
 from sys import argv
 
-
-def main(argv):
-    """
-    Method that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable
-    found in the header of the response
-    """
-    url = argv[1]
-    r = requests.get(url)
-    headers = r.headers.get('X-Request-Id')
-    print(headers)
-
-if __name__ == "__main__":
-    main(argv)
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
